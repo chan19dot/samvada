e53655b027ae02e15c7c352dd25bfad602aa47de
srichandan <srichandan99@gmail.com>
2023-05-27 23:48:45 +0530

    login api added

--- backend/service/authService.js
+++ backend/service/authService.js
@@ -1,14 +1,12 @@
-const { userAuthSchema } = require("../utils/joiSchema");
+const { userAuthSchema, userLoginSchema } = require("../utils/joiSchema");
 const bcrypt = require("bcryptjs");
 const SALTGENROUND = 10;
 const User = require("../models/userModel");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const crypto = require("crypto");
 const validate = async (req, res, next) => {
-  //add joi validation for this schema {email : , password}
-  //bcrypt the password
-  //getByEmailId
-  //compare the password
-  //return policy
+  //use jwt to validate
 };
 
 const register = async (req, res, next) => {
@@ -45,7 +43,51 @@ const register = async (req, res, next) => {
   //save the email and pass in the users table
 };
 
-const login = async (req, res, next) => {};
+const login = async (req, res, next) => {
+  try {
+    const { error, value: cred } = userLoginSchema.validate(req.body);
+    if (error) {
+      throw new Error(error);
+    }
+    //query the data by emailId
+    const userStored = await User.findOne({ emailId: cred.emailId });
+    //bcrypt.compare password
+    const isAuth = await bcrypt.compare(cred.password, userStored.password);
+    if (!isAuth) {
+      err = new Error(error);
+      err.statusCode = 401;
+      err.message = "Password doesn't match";
+      err.status = "UnAuthorized";
+      throw err;
+    }
+    const payload = {
+      username: userStored.userName,
+      exp: Math.floor(Date.now() / 1000) + 1800,
+    };
+    const secretKey = process.env.SECRET_KEY;
+    const token = jwt.sign(payload, secretKey);
+    const refreshToken = generateRefreshToken();
+    res.json({
+      token: token,
+      refreshToken: refreshToken,
+    });
+
+    //return a jwt here
+  } catch (err) {
+    console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "ERROR";
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+};
+const generateRefreshToken = () => {
+  const refreshToken = crypto.randomBytes(64).toString("hex");
+  return refreshToken;
+};
 
 module.exports = {
   register,
